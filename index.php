2700c2aca6dfbcdb593b9c322ad598c8043d78a6
Jonathan Amado <midwayjona@gmail.com>
2020-09-01 04:32:58 -0600

    signup.php

--- index.php
+++ index.php
@@ -2,22 +2,13 @@
 
  -->
 
-
-
 <?php require "assets/header.php" ?>
 
 <div class="main-bg">
-
-
   <main>
-
     <?php require "assets/filler.php" ?>
-
     <?php require "assets/conn.php" ?>
-
   </main>
-
-
-
 </div>
+
 <?php require "assets/footer.php" ?>
--- signup.php
+++ signup.php
@@ -10,31 +10,21 @@
 
 
 <?php require "assets/header.php" ?>
-
 <div class="main-bg">
-
-
   <main>
-
     <div class="wrapper-main">
       <section class="section-default">
         <h1>Signup</h1>
-        <form class="signup-form" action="includes/ignup.inc.php" method="post">
-          <input type="number" name="userid" placeholder="ID">
-          <input type="text" name="username" placeholder="Username">
-          <input type="text" name="mail" placeholder="Mail">
-          <input type="password" name="pwd" placeholder="Password">
-          <input type="password" name="pwd" placeholder="Repeat password">
+        <form class="signup-form" action="includes/signup.inc.php" method="post">
+          <input type="text" name="cid" placeholder="ID" maxlength="13" >
+          <input type="text" name="cuser" placeholder="Username" required>
+          <input type="email" name="cmail" placeholder="Mail" required>
+          <input id="cpwd" name="cpwd" type="password" pattern="^\S{6,}$" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Must have at least 6 characters' : ''); if(this.checkValidity()) form.cpwd_vrfy.pattern = this.value;" placeholder="Password" required>
+          <input id="cpwd_vrfy" name="cpwd_vrfy" type="password" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Please enter the same Password as above' : '');" placeholder="Verify Password" required>
           <button class="btn btn-two" type="submit" name="signup-submit">Signup</button>
         </form>
       </section>
-
     </div>
-
-
   </main>
-
-
-
 </div>
 <?php require "assets/footer.php" ?>
