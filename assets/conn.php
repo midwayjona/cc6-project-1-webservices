59ee9ce8706409f892b175f91b27796531a47c63
Jonathan Amado <midwayjona@gmail.com>
2020-09-01 04:32:27 -0600

    conn.php removed

--- assets/conn.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-try {
-  $conn = new PDO('pgsql:host=localhost;dbname=cc6-pj1', 'midwayjona', 'helloholA1');
-  echo "Connected to PostgreSQL with PDO";
-} catch (PDOException $e) {
-  echo $e->getMessage();
-}
- ?>
