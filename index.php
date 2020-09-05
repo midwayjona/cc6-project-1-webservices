6c1043daac7162364e3e0b9fe574130377cb67de
Jonathan Amado <midwayjona@gmail.com>
2020-09-05 04:12:08 -0600

    Update Index

--- index.php
+++ index.php
@@ -4,10 +4,35 @@
 
 <?php require __DIR__."/assets/header.php" ?>
 
-<div class="main-bg">
-  <main>
-    <?php require __DIR__."/assets/filler.php" ?>
-  </main>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 col-md-7 mx-auto">
+      <?php
+      require __DIR__."/assets/filler.php";
+      if (isset($_SESSION['cid'])) {
+        // code...
+        echo "<p>You are logged in</p>";
+      } else {
+        // code...
+        echo "<p>You are logged out</p>";
+      }
+      ?>
+      <div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+    </div>
+  </div>
 </div>
 
-<?php require __DIR__."/assets/footer.php" ?>
+<?php //require __DIR__."/assets/footer.php" ?>
