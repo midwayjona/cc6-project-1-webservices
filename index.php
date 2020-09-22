cf09774a90ecd146f0559a4260e0a6f607e2f957
Jonathan Amado <midwayjona@gmail.com>
2020-09-21 18:51:08 -0600

    index

--- index.php
+++ index.php
@@ -6,31 +6,9 @@
 
 
 <div class="container">
-  <div class="row">  <div class="col-lg-12 col-md-7 mx-auto">
-      <?php require __DIR__."/assets/filler.php";
-      if (isset($_SESSION['cid'])) {
-        // code...
-        echo "<p>You are logged in</p>";
-      } else {
-        // code...
-        echo "<p>You are logged out</p>";
-      }
-      ?>
-      <div class="card text-center">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-  <div class="card-footer text-muted">
-    2 days ago
-  </div>
-</div>
-
-
+  <div class="row">
+    <div class="col-lg-12 col-md-7 mx-auto">
+      <?php require __DIR__."/assets/filler.php";?>
     </div>
   </div>
 </div>
